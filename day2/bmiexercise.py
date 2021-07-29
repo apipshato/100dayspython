ce4b98b8c89847d71ae33a3b82936273c8ab61fa
apipshato <afifshatos@gmail.com>
2021-07-29 23:42:13 +0700

    Update bmiexercise.py

--- day2/bmiexercise.py
+++ day2/bmiexercise.py
@@ -1,5 +1,7 @@
 height=input("enter your height :\n")
 weight=input("enter your weight :\n")
-bmi=int(weight)/float(height)**2
+
+
+bmi=int(weight) / float(height) ** 2
 bmi_as_int=int(bmi)
 print(bmi_as_int)
