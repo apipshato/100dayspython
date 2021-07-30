a78cdd13e4b390612cbdc19a33971c793d7c595b
apipshato <afifshatos@gmail.com>
2021-07-30 21:50:21 +0700

    Update finalprojectbill.py

--- day2/finalprojectbill.py
+++ day2/finalprojectbill.py
@@ -1,3 +1,10 @@
 print("welcome to the tip calculator")
 bill=float(input("what was the bill $"))
 tip=int(input("how much you give tips  10, 13 or 15"))
+people=int(input("how moany people split the bill"))
+tip_as_percent=tip/100
+total_tip_amount=bill * tip_as_percent
+total_bill=bill + total_tip_amount
+bill_per_person=total_bill / people
+final_amount=round(bill_per_person, 2)
+print(f"each person pay \n ${final_amount}" )
