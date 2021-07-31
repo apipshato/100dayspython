9f82b62cf5bb78444beb423df136ce4d7d510e48
apipshato <afifshatos@gmail.com>
2021-07-31 15:46:18 +0700

    Update BMI-CALCULATOR.py

--- day3/BMI-CALCULATOR.py
+++ day3/BMI-CALCULATOR.py
@@ -1,15 +1,17 @@
 height=float(input("enter your height in m: "))
 weight=float(input("enter your weight in kg: "))
 
-if weight <= 18.5:
+bmi= round(weight / height ** 2)
+
+if bmi < 18.5:
     print("they are under wight")
-    if weight >= 18.5  and weight <= 25:
-        print("they are normal weight")
-    elif weight >=25 and weight <= 30:
-        print("they are overweight")
-    elif weight >= 25:
-        print("they are overweight")
-    elif weight <= 30:
-        print("they are overweight")
-    
-        
+    if bmi > 18.5  and bmi < 25:
+        print(f"your bmi{bmi} they are normal weight")
+    elif bmi >25 and bmi < 30:
+        print(f"your bmi {bmi}they are overweight")
+    elif bmi > 30 and bmi < 35:
+        print(f"your bmi {bmi}they are obess")
+    elif bmi > 35:
+        print(f"your bmi {bmi} they are clinicaly obess")
+else:
+    print(f"your bmi {bmi}, your clinically obese danger ")
