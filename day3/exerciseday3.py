51154a257fb1395d135cee72f8ca2d4c3e7c81a7
apipshato <afifshatos@gmail.com>
2021-07-31 13:12:55 +0700

    day 3

---
+++ day3/exerciseday3.py
@@ -0,0 +1,7 @@
+print("welcome to rollercoster")
+heigth=int(input("what is your height in cmn?"))
+
+if heigth > 120:
+    print("you can ride roller coster")
+else:
+    print("you cannot play roller coster")
---
+++ day3/exerciziseday3-2.py
@@ -0,0 +1,8 @@
+print("welcome to rollercoster")
+height=input("what is your tall? \n")
+if height > 120:
+    print("he can play rollercoster")
+    age =input("what is your age")
+    if age <=18:
+        
+    
