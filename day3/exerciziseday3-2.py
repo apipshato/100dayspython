973de9219fadb9d687c2b70d8bc29e7b285b3edd
apipshato <afifshatos@gmail.com>
2021-07-31 13:48:58 +0700

    learn about line space

--- day3/exerciziseday3-2.py
+++ day3/exerciziseday3-2.py
@@ -1,8 +1,14 @@
 print("welcome to rollercoster")
 height=input("what is your tall? \n")
+
 if height > 120:
     print("he can play rollercoster")
-    age =input("what is your age")
-    if age <=18:
-        
-    
+    age = int(input("what is your age "))
+    if age <12:
+        print("please pay $5")
+    elif age <= 18:
+        print("please play $7")
+    else:
+     print("pelase pay $12")
+else:
+ print("sorryy, you can ride")
