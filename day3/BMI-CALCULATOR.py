135dfdcbf02e04808bd55c0ea4e68589a38c2dc9
apipshato <afifshatos@gmail.com>
2021-07-31 15:54:20 +0700

    clear line

--- day3/BMI-CALCULATOR.py
+++ day3/BMI-CALCULATOR.py
@@ -5,13 +5,13 @@ bmi= round(weight / height ** 2)
 
 if bmi < 18.5:
     print("they are under wight")
-    if bmi > 18.5  and bmi < 25:
-        print(f"your bmi{bmi} they are normal weight")
-    elif bmi >25 and bmi < 30:
-        print(f"your bmi {bmi}they are overweight")
-    elif bmi > 30 and bmi < 35:
-        print(f"your bmi {bmi}they are obess")
-    elif bmi > 35:
-        print(f"your bmi {bmi} they are clinicaly obess")
+elif bmi > 18.5  and bmi < 25:
+    print(f"your bmi{bmi}, they are normal weight")
+elif bmi >25 and bmi < 30:
+    print(f"your bmi {bmi} ,they are overweight")
+elif bmi > 30 and bmi < 35:
+    print(f"your bmi {bmi} ,they are obess")
+elif bmi > 35:
+        print(f"your bmi {bmi}, they are clinicaly obess")
 else:
     print(f"your bmi {bmi}, your clinically obese danger ")
