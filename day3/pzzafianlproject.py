438e7847c38bbb2b50bf9f24e32795708653f975
apipshato <afifshatos@gmail.com>
2021-08-02 14:36:06 +0700

    Create pzzafianlproject.py

---
+++ day3/pzzafianlproject.py
@@ -0,0 +1,5 @@
+print("welcome to pizza Pyton delivers")
+size= input("what size pizza do you want ? S M OR L ")
+add_paperoni=input("do you want paperoni? y or n? ")
+extra_chesse=input("do you want cheese? ")
+
