81c2a77eda0e1e04d71a09be2bc04894eb6de218
apipshato <afifshatos@gmail.com>
2021-08-02 14:49:44 +0700

    Update pzzafianlproject.py

--- day3/pzzafianlproject.py
+++ day3/pzzafianlproject.py
@@ -3,3 +3,19 @@ size= input("what size pizza do you want ? S M OR L ")
 add_paperoni=input("do you want paperoni? y or n? ")
 extra_chesse=input("do you want cheese? ")
 
+
+bill=0
+if size == "S":
+  bill += 15
+elif size == "M":
+    bill+= 20
+elif size == "L":
+    bill += 25
+
+if add_paperoni == "Y":
+    if  size == "S":
+        bill += 2
+    else:
+        bill +=3
+
+
