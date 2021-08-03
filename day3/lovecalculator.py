cb490e616456beeb87400b5b0f4cdaab54b3c084
apipshato <afifshatos@gmail.com>
2021-08-04 01:04:41 +0700

    Update lovecalculator.py

--- day3/lovecalculator.py
+++ day3/lovecalculator.py
@@ -5,6 +5,19 @@ name2=input("what is your second name: \n")
 combine_string= name1 + name2
 lower_case_string=combine_string.lower()
 t=lower_case_string.count("t")
-t=lower_case_string.count("r")
-t=lower_case_string.count("u")
-t=lower_case_string.count("e")
+r=lower_case_string.count("r")
+u=lower_case_string.count("u")
+e=lower_case_string.count("e")
+true=t + r +u +e
+
+l=lower_case_string.count("l")
+o=lower_case_string.count("o")
+v=lower_case_string.count("v")
+e=lower_case_string.count("e")
+
+love= l + o + v + e
+
+love_score= str(true) + str(love)
+print(love_score)
+
+if (love_score < 10) or (love_score >90):
