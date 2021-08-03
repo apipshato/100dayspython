91e235c70052bff3d4b534516922ddc4d99ad7eb
apipshato <afifshatos@gmail.com>
2021-08-04 00:55:44 +0700

    Update lovecalculator.py

--- day3/lovecalculator.py
+++ day3/lovecalculator.py
@@ -3,3 +3,8 @@ name1=input("what is your name: \n")
 name2=input("what is your second name: \n")
 
 combine_string= name1 + name2
+lower_case_string=combine_string.lower()
+t=lower_case_string.count("t")
+t=lower_case_string.count("r")
+t=lower_case_string.count("u")
+t=lower_case_string.count("e")
