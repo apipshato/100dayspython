0189eba5f6eace618ea67d0f7496b01bcba1da74
apipshato <afifshatos@gmail.com>
2021-08-04 13:16:55 +0700

    Update finalproject.py

--- day3/finalproject.py
+++ day3/finalproject.py
@@ -1,6 +1,9 @@
 print("welcome to treasure island")
 print("your misson is to find the treasure")
-choice=input("your at crrosroad, where do you want to go? type left or right: \n")
+choice = input(
+    "your at crrosroad, where do you want to go? type left or right: \n")
 
 if choice == "left":
-    input("you come to lake ")
+    input('you come to lake , there is ilandin the middle lake . type "wait" to for boat, type "swim" to wim accross')
+else:
+    print("you fell into the hole, game over")
