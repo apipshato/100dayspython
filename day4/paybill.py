36b5b18f97e8fb12d905e63b4dea37a98b3a7632
apipshato <afifshatos@gmail.com>
2021-08-04 15:10:02 +0700

    Update paybill.py

--- day4/paybill.py
+++ day4/paybill.py
@@ -3,5 +3,6 @@ name_string=input("give me everybody name, separeted by comma")
 names=name_string.split(", ")
 numes_item=len(names)
 random_choice=random.randint(0, numes_item -1)
-print(random_choice)
+person_who_pay=names(random_choice)
+print(person_who_pay + "going to buy meal todaya")
 
