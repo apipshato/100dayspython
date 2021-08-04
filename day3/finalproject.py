71dbfd9802c19b953b331f350067857f72b12331
apipshato <afifshatos@gmail.com>
2021-08-04 13:09:12 +0700

    Create finalproject.py

---
+++ day3/finalproject.py
@@ -0,0 +1,6 @@
+print("welcome to treasure island")
+print("your misson is to find the treasure")
+choice=input("your at crrosroad, where do you want to go? type left or right: \n")
+
+if choice == "left":
+    input("you come to lake ")
