3d99f8542492a3ccc78b23609bb236bd45a7d68e
apipshato <afifshatos@gmail.com>
2021-08-04 21:22:11 +0700

    Update loopsday5-2.py

--- day5/loopsday5-2.py
+++ day5/loopsday5-2.py
@@ -1,4 +1,21 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+total_height=0
+for height in student_heights:
+    total_height += height
+    print(total_height)
+
 #Write your code below this row 👇
 
-for _ in range(100):
-    print(_)
+number_of_student=0
+for student in student_heights:
+    number_of_student += 1
+# total_height=sum(student_heights)
+# number_of_student=len(student_heights)
+avarage_height=round(total_height / number_of_student)
+print(avarage_height)
+
+
