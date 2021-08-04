007a77c4a6719504e9255abd210a45d8303e59f7
apipshato <afifshatos@gmail.com>
2021-08-04 15:06:02 +0700

    Update paybill.py

--- day4/paybill.py
+++ day4/paybill.py
@@ -1,3 +1,7 @@
 import random
 name_string=input("give me everybody name, separeted by comma")
 names=name_string.split(", ")
+numes_item=len(names)
+random_choice=random.randint(0, numes_item -1)
+print(random_choice)
+
