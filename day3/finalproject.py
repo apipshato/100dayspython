ad5a2bb3491c9f400fdb57ca9406179abb6db863
apipshato <afifshatos@gmail.com>
2021-08-04 13:52:45 +0700

    Update finalproject.py

--- day3/finalproject.py
+++ day3/finalproject.py
@@ -6,15 +6,16 @@ choice1 = input(
 if choice1 == "left":
    choice2= input('you come to lake , there is ilandin the middle lake . type "wait" to for boat,type "swim" to wim accross').lower()
    if choice2 == "wait":
-       choice3=input("you arrive at islan unharmed,  there is house 3 doors one red, one yellow one blue")
+       choice3=input("you arrive at islan unharmed,  there is house 3 doors one red, one yellow one blue").lower()
        if choice3 == "red":
             print("your room full of fire")
        elif choice3 == "yellow":
             print("ypu found the treasure you wins")
        elif choice3 =="blue":
             print("you enter room of beast you lose")
+       else:
+          print("you you choice a door doesnt exist game oveer")
 else:
- print("you got attacked by an angry trout, game over")
-
-else:
-    print("you fell into the hole, game over")
+  print("you got attacked by an angry trout, game over")
+ 
+ 
