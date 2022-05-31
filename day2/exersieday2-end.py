c0d4488849175a714a74e8358daa9e7960a7798a
apipshato <afifshatos@gmail.com>
2022-05-31 10:50:10 +0700

    Update exersieday2-end.py

--- day2/exersieday2-end.py
+++ day2/exersieday2-end.py
@@ -1,6 +1,10 @@
-two_digit_number=input("type to digit  number:")
-first_digit=two_digit_number[0]
-second_digit=two_digit_number[1]
-result=int(first_digit )+ int(two_digit_number)
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
 
+####################################
+#Write your code below this line 👇
+first_digit_number=two_digit_number[0]
+second_digit_number=two_digit_number[1]
+result=int(first_digit_number) + int(second_digit_number)
 print(result)
