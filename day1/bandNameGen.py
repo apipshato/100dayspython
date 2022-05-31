93ca8fe46efac33a5fdc64239ddd2222feda53c5
apipshato <afifshatos@gmail.com>
2022-05-31 10:03:37 +0700

    add final project day 1

---
+++ day1/bandNameGen.py
@@ -0,0 +1,4 @@
+print('welcome to band name generator')
+street=input("what's name of city you grew up in? \n")
+pet=input("what's is your pet name?\n")
+print("your band name is" + street + " " + pet)
