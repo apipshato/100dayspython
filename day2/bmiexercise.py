be1514141fe87751b510cdde92f00b7d293e9011
apipshato <afifshatos@gmail.com>
2022-06-07 10:03:16 +0700

    add exercise

--- day2/bmiexercise.py
+++ day2/bmiexercise.py
@@ -1,7 +1,19 @@
-height=input("enter your height :\n")
-weight=input("enter your weight :\n")
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+weight_as_int = int(weight)
+height_as_float = float(height)
 
 
-bmi=int(weight) / float(height) ** 2
-bmi_as_int=int(bmi)
+
+# Using the exponent operator **
+bmi = weight_as_int / height_as_float ** 2
+
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+
 print(bmi_as_int)
--- day2/finalexercise.py
+++ day2/finalexercise.py
@@ -1,10 +1,14 @@
-age=input("what is your current age")
 
-age_as_int=int(age)
-years_remaining=90 - age_as_int
-day_remaining=years_remaining * 365
-week_remaining=day_remaining * 52
-month_remaiinig=week_remaining * 12
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
 
-message=f"you have {day_remaining} days, {week_remaining} weeks, and {month_remaiinig} month left"
-print(message)
+#Write your code below this line 👇
+
+years = 90 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
