f054a1c5075b8e52d673e8d880dce690e62c80c5
apipshato <afifshatos@gmail.com>
2022-06-07 20:56:41 +0700

    bebe

--- day3/elifnest.py
+++ day3/elifnest.py
@@ -1,13 +1,23 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill=0
+
 if height >= 120:
   print('can ride')
   age=int(input("what is your age"))
   if age >= 12:
-    print('$5')
+    bill=5
+    print('child ticket all $5')
   elif age <= 18:
-    print('$12')
+    bill=7
+    print('adult ticekt all $7')
   else:
+    bill=12
     print('$7')   
+    
+  wants_photo=input("do you wanna photos? Y or N")
+  if wants_photo == 'Y':
+    bill += 3
+    print(f"your final {bill}")
 else:
   print('cannot ride')
--- day3/lovecalculator.py
+++ day3/lovecalculator.py
@@ -2,22 +2,23 @@ print("welcome to calcluator love")
 name1=input("what is your name: \n")
 name2=input("what is your second name: \n")
 
-combine_string= name1 + name2
+
+
+combine_string=name1 + name2
 lower_case_string=combine_string.lower()
 t=lower_case_string.count("t")
 r=lower_case_string.count("r")
 u=lower_case_string.count("u")
 e=lower_case_string.count("e")
-true=t + r +u +e
+true=t + r + u + e
 
-l=lower_case_string.count("l")
-o=lower_case_string.count("o")
-v=lower_case_string.count("v")
-e=lower_case_string.count("e")
+l=lower_case_string('l')
+o=lower_case_string("o")
+v=lower_case_string("v")
+e=lower_case_string("e")
+love=l+o+v+e
 
-love= l + o + v + e
-
-love_score= int(str(true) + str(love))
+love_score=int(str(true)) + int(str(love))
 print(love_score)
 
 if (love_score < 10) or (love_score >90):
@@ -26,3 +27,22 @@ elif (love_score >= 40) or (love_score <= 50):
     print(f"your love score {love_score},you cannot go toghether")
 else:
     print(f"your score is {love_score}")
+# combine_string= name1 + name2
+# lower_case_string=combine_string.lower()
+# t=lower_case_string.count("t")
+# r=lower_case_string.count("r")
+# u=lower_case_string.count("u")
+# e=lower_case_string.count("e")
+# true=t + r +u +e
+
+# l=lower_case_string.count("l")
+# o=lower_case_string.count("o")
+# v=lower_case_string.count("v")
+# e=lower_case_string.count("e")
+
+# love= l + o + v + e
+
+# love_score= int(str(true) + str(love))
+# print(love_score)
+
+
