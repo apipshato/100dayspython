6de4ebc28f9b3b1a27bd56adc5f378ec3e59312c
apipshato <afifshatos@gmail.com>
2022-06-20 14:52:21 +0700

    Update paybill.py

--- day4/paybill.py
+++ day4/paybill.py
@@ -6,5 +6,5 @@ names=name_string.split(", ")
 numes_item=len(names)
 random_choice=random.randint(0, numes_item -1)
 person_who_pay=names[random_choice]
-print(person_who_pay + "going to buy meal todaya")
+print(person_who_pay + "going to buy meal today")
 
